1227a3770facc68ec183f9683da34b460bd4086e
tanu2474 <ahertanvi462@gmail.com>
2026-02-07 14:36:34 +0530

    TODO Items added

--- index.js
+++ index.js
@@ -4,22 +4,57 @@ app.use(express.json());
 const PORT = 5000;
 
 const TODO_ITEMS = [
-    "Sample Todo Item",
-    "Another Todo Item",
-    "Yet Another Todo Item"
+    {
+        id:1,
+        todoItem:"Learn Node.js",
+        priority:"High",
+        emoji:"📚",
+        isDone:false,
+        createAt:new Date().toISOString(),
+    },
+    {
+        id:2,
+        todoItem:"Buy Pen",
+        priority:"medium",
+        emoji:"🖊️",
+        isDone:false,
+        createAt:new Date().toISOString(),
+    },
+    {
+        id:3,
+        todoItem:"Go for a walk",
+        priority:"low",
+        emoji:" 🚶‍♂️",
+        isDone:false,
+        createAt:new Date().toISOString(),
+    }
+
+    
 ];
- app.get('/todos', (req, res) => {
-    res.json({
-        success: true,
-        todos: TODO_ITEMS,
-        message: "Todos fetched successfully"
+ app.get('/todos', (req, res) => {    
+        res.json({
+        data:TODO_ITEMS,
+        message: "Todos fetched successfully",
     });
 });
 
+
 app.post('/todos', (req, res) => {  
   //req.body {todo: "New Todo Item"}
-  const { todo } = req.body;
-  TODO_ITEMS.push(toto);
+  const { todo , priority,emoji } = req.body;
+
+  
+  const todoObj = {
+    id: TODO_ITEMS[TODO_ITEMS.length - 1].id + 1 ,
+    todoItem:todo,
+    priority:priority,
+    emoji:emoji,
+    isDone:false,
+    createAt:new Date().toISOString()
+
+  };
+
+  TODO_ITEMS.push( todoObj);
 
     res.json({
         success: true,
